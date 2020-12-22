3a554c1ae073776dbf9498194be6a0fbd9f8ee39
SamPM <63992604+SamPM092@users.noreply.github.com>
2020-12-22 11:53:28 +0700

    Update index.php

--- konten/index.php
+++ konten/index.php
@@ -4,6 +4,7 @@ require 'header.php';
 
 ?>
 
+<!--
 <script>
 function myFunction1() {
   document.getElementById("paragraph").innerHTML = "Paragraph changed.";
@@ -21,13 +22,13 @@ function myFunction5() {
   document.getElementById("paragraph").innerHTML = "Paragraph changed5.";
 }
 </script>
-
+-->
 <!-- JIKA USER LEVEL = MAHASISWA-->
 <h1> Selamat Datang (Nama Mahasiswa) </h1>
 <h5> Dengan NIM (NIM) </h4>
 <a href="chat_dosen.php"> (Ambil nama dosen dari database) </a>
 
-<div class="tugas">
+<!-- <div class="tugas">
   <span class="list_tugas"><a type="button" onclick="myFunction1()"> 1 </a></span>
   <span class="list_tugas"><a type="button" onclick="myFunction2()"> 2 </a></span>
   <span class="list_tugas"><a type="button" onclick="myFunction3()"> 3 </a></span>
@@ -36,4 +37,30 @@ function myFunction5() {
 </div>
 <div>
 <p id="paragraph"> KONSEPNYA BEGINI TAPI MASIH SALAH</p>
-</div>
+</div> -->
+
+<h5><a href=" http://wa.me/085265067964">Chat dengan Dosen</a></h5>
+<h5> Proggres : <a href="bimbingan.php">progress</a></h5>
+<h5><a href="bimbingan.php">Bimbingan</a></h5>
+<h5><a href="exum.php">Exum</a></h5>
+<h5><a href="seminar_proposal.php">Seminar Proposal</a></h5>
+<h5><a href="hasil_penelitian.php">Hasil Penelitian(buat skripsi</a></h5>
+<h5><a href="semhas.php">Seminar Hasil</a></h5>
+<h5><a href="sidang.php">Sidang (Meja Hijau)</a></h5>
+<h5><a href="lulus.php">Lulus</a></h5>
+ <!-- <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> Bimbingan </label><br>
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <label for="vehicle2"> Exum </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Seminar Proposal </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Hasil Penelitian </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Hasil Penelitian </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Seminar Hasil </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Sidang </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Lulus </label><br> -->
