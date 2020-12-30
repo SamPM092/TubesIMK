e8ebc0b47f1e2ac296654538041a7d1b4c485f28
SamPM <63992604+SamPM092@users.noreply.github.com>
2020-12-30 16:25:39 +0700

    Adding Halaman Utama
    Halaman Utama sebelum login

---
+++ halaman_utama.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/halaman.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-sm bg-dark">
+      <a class="nav-link" href="https://fasilkom-ti.usu.ac.id/">
+        <img src="css/logo.png">
+      </a>
+      <div class="login_button">
+  <ul class="navbar-nav">
+    <li class="nav-item1">
+      <a class="nav-link" href="index.php">LOGIN</a>
+    </li>
+  </ul>
+</div>
+</nav>
+    <div class="konten">
+      <div class="border">
+        <div class="isi">
+          <h2><center>SELAMAT DATANG DI WEB BIMBINGAN ONLINE</center></h2>
+          <p>Tahapan Kelulusan :</p>
+          <p>
+          2.1 Menentukan Topik Skripsi dan Dosen Pembimbing <br>
+          Topik skripsi dapat diambil dari jurnal-jurnal terbaru mengenai jurusan.
+          Setelah menentukan topik, lalu menyusun proposal penelitian untuk selanjutnya di-acc oleh ketua prodi dan akhirnya dapat memulai penyusunan skripsi.
+          <p>
+          2.2 Seminar Usulan Penelitian <br>
+          Seminar usulan penelitian memresentasikan adalah skripsi Bab I sampai Bab III yang memuat latar belakang penelitian, teori atau pustaka yang dipakai, serta alur penelitian yang akan dilakukan. Dalam seminar tersebut, dosen pembimbing akan menguji dan memberi saran tentang penelitian sampai akhirnya nanti dapat dieksekusi.
+
+          Setelah lulus seminar usulan penelitian, hal selanjutnya yang dilakukan adalah mulai mengeksekusi penelitian. Hasil penelitian akan tertulis pada Bab IV dan simpulan penelitian akan ditulis pada Bab V.
+          </p>
+          <p>
+          2.3 Kolokium<br>
+          Kolokium merupakan presentasi hasil penelitian kita di hadapan dosen pembimbing dan audiens lain yang biasanya adalah para mahasiswa juga. Untuk melakukan tahap ini, penyusunan skripsi harus sudah selesai tetapi tidak harus sempurna.
+          </p>
+          <p>
+          2.4 Sidang Skripsi<br>
+          Di tahap ini, dengan pakaian formal, kita akan diuji oleh beberapa dosen penguji yang telah ditentukan sebelumnya apakah kita layak atau tidak menjadi sarjana. Isinya, tentunya, adalah presentasi skripsi. Selain itu, ada juga sidang komprehensif yang isinya adalah pengujian tentang materi-materi selama kuliah S-1 kepada mahasiswa.
+
+          Sidang skripsi ini akan menjadi mata kuliah terakhir yang kita dapat dan menjadi penentu akhir IPK final.
+          </p>
+          <p>
+          2.5 Yudisium<br>
+          Yudisium adalah pernyataan resmi dari kaprodi bahwa kita telah diluluskan dan resmi menjadi sarjana. Disebutkan pula predikat kelulusan serta IPK akhir kita. Setelah diyudisium, kita telah resmi menjadi sarjana dan tanggal yudisium ini akan tercantum di ijazah.
+          </p>
+          <p>
+          2.6 Wisuda<br>
+          Wisuda ini sifatnya lebih ke seremoni atau perayaan kelulusan saja. Jadwalnya tidak selalu berdekatan dengan yudisium.  Wisuda ini jugalah yang menandai akhir petualangan kita sebagai mahasiswa S-1 walaupun sebenarnya, kita sudah menjadi sarjana setelah yudisium.</p>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
